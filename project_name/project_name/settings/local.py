ab7bedb8a03422ee1c6a964d40a23297bda7a905
Janez K <janez.kranjc@gmail.com>
2014-01-05 19:09:42 +0100

    popravki

--- project_name/project_name/settings/local.py
+++ project_name/project_name/settings/local.py
@@ -9,13 +9,17 @@ DEBUG_TOOLBAR = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_DIR, '{{project_name}}', '{{project_name}}.db'),                    
-        'USER': '',                   
-        'PASSWORD': '',              
-        'HOST': '',                 
-        'PORT': '',                
+        'NAME': os.path.join(PROJECT_DIR, '{{project_name}}', '{{project_name}}.db'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
+
+if DEBUG:
+    # Show emails in the console during developement.
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 if DEBUG_TOOLBAR:
     INTERNAL_IPS = ('127.0.0.1',)
