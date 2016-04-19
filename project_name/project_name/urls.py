e304b52370c97c1bed47f2f1e767543fb64ddff6
Janez K <janez.kranjc@gmail.com>
2016-04-19 10:03:50 +0200

    django 1.9.5

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+import django.views.static
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', '{{project_name}}.views.home', name='home'),
     # url(r'^{{project_name}}/', include('project_name.foo.urls')),
@@ -15,10 +16,10 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-)
+]
 
 if settings.DEBUG:
-    urlpatterns += patterns('',
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', django.views.static.serve, {
             'document_root': settings.MEDIA_ROOT,
-        }))
+        })]
